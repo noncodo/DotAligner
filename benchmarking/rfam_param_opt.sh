7907e16a90e7a2e56c6386ee612572691f0579de
Martin Smith <martinsmith@Martins-MacBook-Pro.local>
2016-06-03 11:12:37 +1000

    updated benchmarking scripts

--- benchmarking/rfam_param_opt.sh
+++ benchmarking/rfam_param_opt.sh
@@ -22,16 +22,51 @@ module load gi/ViennaRNA/2.1.3 marsmi/dotaligner/0.3
 #   done
 # done > $1/pairwise_list.txt
 # >&2 echo  
-# cut -d "/" -f 3 $1/file_list.txt | cut -d "_" -f 1-2 > $1/structure_ids.txt
+
+
+# this is needed for R code
+#cut -d "/" -f 3 $1/file_list.txt | cut -d "_" -f 1-2 > $1/structure_ids.txt
 if [[ ! -d $1/logs ]]; then mkdir $1/logs ; fi
 if [[ ! -d $1/bench ]]; then mkdir $1/bench ; else rm $1/bench/* ; fi
+#if [[ ! -d $1/bench ]]; then mkdir $1/bench ; fi
+if [[ -e $1/bench/cmds.list ]]; then rm $1/bench/cmds.list ; rm $1/bench/out.list ; fi
 
-for k in `seq 0.25 0.25 0.75`; do  
- for t in `seq 0.25 0.25 0.75`; do 
-  for s in 5000 1000 200; do  
+####################
+# INITIAL COMMANDS
+####################
+
+# for k in `seq 0.25 0.25 0.75`; do  
+#  for t in `seq 0.25 0.25 0.75`; do 
+#   for s in 5000 1000 200; do  
+#    for T in 10 5 1; do
+#     for o in 0.75 1; do  
+#      for e in 0.05 0.2; do
+#       CMD="DotAligner -T ${T} -s ${s} -k ${k} -t ${t} -o ${o} -e ${e} "
+#       PARAMS="T-${T}_s-${s}_k-${k}_t-${t}_o-${o}_e-${e}"
+#       echo $CMD >> $1/bench/cmds.list
+# 	  echo $PARAMS >> $1/bench/out.list
+# 	 done
+# 	done
+#    done
+#   done
+#  done
+# done 
+# echo "Generated "`wc -l $1/bench/cmds.list`" commands"
+
+
+####################
+# REFINED COMMANDS
+####################
+# Optimal parameters from above: 
+# high_pi: T-10_s-200_k-0.50_t-0.75_o-1_e-0.05	
+# low_pi: T-10_s-200_k-0.25_t-0.5_o-1_e-0.05	
+
+for k in 0.3 0.4 0.5 0.6 ; do  
+ for t in 0.5 0.6 0.7 0.8 ; do 
+  for s in 1 5 20 50 ; do
    for T in 10 5 1; do
-    for o in 0.75 1; do  
-     for e in 0.05 0.2; do
+    for o in 1; do  
+     for e in 0.05 ; do
       CMD="DotAligner -T ${T} -s ${s} -k ${k} -t ${t} -o ${o} -e ${e} "
       PARAMS="T-${T}_s-${s}_k-${k}_t-${t}_o-${o}_e-${e}"
       echo $CMD >> $1/bench/cmds.list
@@ -43,6 +78,7 @@ for k in `seq 0.25 0.25 0.75`; do
  done
 done 
 echo "Generated "`wc -l $1/bench/cmds.list`" commands"
+
 
 timestamp=$( date +"%d%m%y-%H%M" )
 #CMD=$( echo qsub -cwd -V -S /bin/bash  -o $1/bench_qsub.out.${timestamp} -e $1/bench_qsub.err.${timestamp} -P RNABiologyandPlasticity -t 1:`wc -l ${1}/bench/cmds.list` -V -cwd -N bench_low -l mem_requested=1G,h_vmem=2G -j y -b y ./bench_it_high.sge  )
@@ -58,19 +94,8 @@ CMD="qsub -cwd -V -S /bin/bash \
 	-j y -b y \
 	./da_bench_${1}.sge" 
 #	-t 1:`wc -l ${1}/bench/cmds.list`
-(echo "[COMMAND]"; echo $CMD) && $CMD 
-#echo $CMD 
-
-#qsub  -t 1:`wc -l ${1}/bench/cmds.list` -V -cwd -N bench_high -l mem_requested=1G,h_vmem=2G -j y -b y ./bench_it_high.sge
-
-# 
-# if [[ -e ${1}/qsub.dotalnr.out ]] ; then rm ${1}/qsub.dotalnr.out ; fi 
-# if [[ -e ${1}/qsub.dotalnr.err ]] ; then rm ${1}/qsub.dotalnr.err ; fi 
-# CMD="qsub -V -cwd -P ${GRID_ACCOUNT} -N DotAlnR -o ${1}/logs/dotaligner.$(date +"%d%m%y-%H%M").out -e ${1}/logs/dotaligner.$(date +"%d%m%y-%H%M").err -pe smp 1"
-# CMD=${CMD}" -l mem_requested=1256M,h_vmem=1512M,h=!(epsilon-0-24.local|epsilon-0-25.local) -t 1:$((1+${NUMPW}/$LINESperFILE)) -b y ./dotaligner.sge ${1}"
-# DOTALNR=$( $CMD ) 
-# echo "launching: "$CMD 
-# echo $DOTALNR
-# JID=$( echo ${DOTALNR} | cut -d " " -f 3 | cut -d "." -f 1 )
-# qsub -hold_jid $JID -V -o ./$1/logs/dotaligner.$(date +"%d%m%y-%H%M").clean -cwd -N clean_dotalnr -pe smp 1 -l mem_requested=1G,h_vmem=1G -j y -b y ./cleanup.sge $1 dotaligner
+(echo "[COMMAND]"; echo $CMD) && DOTALNR=$( $CMD ) 
+echo $DOTALNR
+JID=$( echo ${DOTALNR} | cut -d " " -f 3 | cut -d "." -f 1 )
+qsub -hold_jid $JID -V -o ./$1/logs/bench.$(date +"%d%m%y-%H%M").clean -cwd -N clean_dotalnr -pe smp 1 -l mem_requested=1G,h_vmem=1G -j y -b y ./cleanup_bench.sge $1 bench
 

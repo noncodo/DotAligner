a841169fcff7ff1559f7d993d131d5a7d1916586
noncodo <martinalexandersmith@gmail.com>
2016-11-10 11:44:26 +1100

    added fixed scripts

--- analysis/analysis/launcher.sh
+++ analysis/analysis/launcher.sh
@@ -25,7 +25,6 @@ GRID_ACCOUNT="RNABiologyandPlasticity"
 #################################################################
 #	                        FUNCTIONS
 #################################################################
-logo() {
 >&2 echo -e "\e[91m             \`\`.:/osyyyys+:\`            "
 >&2 echo -e "         \`.--:::::--::+oymNNms:         "
 >&2 echo -e "       .-::-.-/sssyyyyyys++odNMm+\`      "
@@ -45,46 +44,79 @@ logo() {
 >&2 echo -e "         :smMNNNNMMNNNNNNNNNds:         "
 >&2 echo -e "            \`:+shddmmddhs+:\`            \e[39m"
 >&2 echo -e                            
-}
-
-foldit () {
- #>&2 echo -e "[DEBUG] Funciton WD = "$( pwd ) 
- >&2 echo -ne "[*] generating RNA base-pairing probability matrices "
- >&2 echo -ne "(\e[31mbe patient\e[39m)..." 
- ## TO DO  split file into $MAXCPU subfiles to speed this up
- $RNAFOLDBIN -p --noPS --noLP < $1 > ../logs/RNAfold.log
- >&2 echo -e "[ \e[92mDONE\e[39m ]"  
-}
-
 
 #################################################################
 #                 Fold RNA to get dotplots
 #################################################################
 ## TO DO : parallelize to make it faster 
->&2 echo -e "[DEBUG] BASEDIR= "$BASEDIR
 if [[ ! -d $BASEDIR/logs ]]; then mkdir -p $BASEDIR/logs ; fi
+#split fasta into smaller files
+>&2 echo -ne "[*] Splitting fasta file ..."
+if [[ ! -d ${BASEDIR}/split ]]; then mkdir -p $BASEDIR/split ; fi
+cd $BASEDIR/split
+NUMSEQ=$(wc -l ${BASEDIR}/../$1 | awk '{print $1}')
+LINESperFILE=$(( ${NUMSEQ} / ${MAXGRIDCPU} ))
+(( LINESperFILE+=1 ))
+split -l $LINESperFILE ${BASEDIR%/*}/$1
+  >&2 echo -e "[ \e[92mDONE\e[39m ]"  
+
+#prepare folding command
+  CMD="qsub -V -terse -sync y -cwd -P ${GRID_ACCOUNT} -N foldit 
+ -o $BASEDIR/logs/foldit."$(date +"%d%m%y-%H%M")".out 
+ -e $BASEDIR/logs/foldit."$(date +"%d%m%y-%H%M")".err 
+ -pe smp 1 
+ -t 1:${MAXGRIDCPU}
+ -l mem_requested=1G,h_vmem=1G
+ -b y -S /bin/bash
+ ../foldit.sge"
 cd $BASEDIR	&& >&2 echo -e "[*] working in BASEDIR: "$(pwd)
 >&2 echo -ne "[*] Evaluating folder contents ..."
+counter="-1"
 if [[ ! -d ps ]]; then 
+	mkdir ps
   	>&2 echo -e "no RNA structures found"
-	mkdir ps && cd ps
-	foldit ${BASEDIR%/*}/$1
+  	>&2 echo -e "[*] Generating pairwise probabilities..."
+	$CMD | while read line ; do 
+  		counter=$(( $counter + 1 ))
+  		progress=$(( 100 * $counter / $MAXGRIDCPU ))
+  		echo -ne "\r... "$progress"% complete" 
+  	done
+ 	>&2 echo -e "\r[*] successfully completed command: "
+ 	>&2 echo -e "\e[31m"$CMD"\e[39m" 
 elif [[ $( ls ps | wc -l ) -ne $( grep ">" ../${1} | wc -l ) ]]; then 
 	>&2 echo -e "incomplete amount of structures"
-	cd ps
-	foldit ${BASEDIR%/*}/$1
+	cd $BASEDIR	
+	>&2 echo -e "[*] Generating pairwise probabilities..."
+	$CMD | while read line ; do 
+  		counter=$(( $counter + 1 ))
+  		progress=$(( 100 * $counter / $MAXGRIDCPU ))
+  		echo -ne "\r... "$progress"% complete" 
+  	done
+ 	>&2 echo -e "\r[*] successfully completed command: "
+ 	>&2 echo -e "\e[31m"$CMD"\e[39m" 
 else
 	>&2 echo -e "found existing RNA structures "
   	>&2 echo -ne "    \e[31mDo you want to refold them?\e[39m [y/N] "
 	read -n1 redo 
 	>&2 echo 
 	if [[ $redo != 'n' && $redo != 'N' ]]; then 
- 		cd ps && foldit ${BASEDIR%/*}/$1
- 		cd ..
+ 		cd $BASEDIR	
+ 		>&2 echo -e "[*] Generating pairwise probabilities..."
+		$CMD | while read line ; do 
+  		counter=$(( $counter + 1 ))
+  		progress=$(( 100 * $counter / $MAXGRIDCPU ))
+  		echo -ne "\r... "$progress"% complete" 
+  	done
+ 	>&2 echo -e "\r[*] successfully completed command: "
+ 	>&2 echo -e "\e[31m"$CMD"\e[39m" 
  	fi
 fi
 cd $BASEDIR
-
+rm -rf split
+# Seems like some sequences don't produce .ps files based on 
+# line count differences between file_list.txt and fasta header count
+# Does not affect clustering, but s few sequences may be missed
+# SGE bug or RNAfold error? Folds w/ ViennaRNA-2.2.10
 
 #################################################################
 #	                  Convert .ps to .pp 
@@ -235,7 +267,7 @@ awk 'OFS="\t"{ print $1,$2,$3,$4,1-$5 }'  scores_normalized.tsv \
 >&2 echo - "[*] preparing cluster analysis..."
 cat > clustering.R << EOF
 #!/usr/bin/Rscript
-loadPackage <- function(x){
+loadPackages <- function(x){
   for( i in x ){
     #  require returns TRUE invisibly if it was able to load package
     if( ! require( i , character.only = TRUE ) ){
@@ -250,7 +282,7 @@ loadPackage <- function(x){
 }
 cat("[R] loading required packages...", stderr())
 suppressMessages( suppressWarnings(
-    loadPackage( c("gplots","dbscan") ) ) ) 
+    loadPackages( c("gplots","dbscan","RcolorBrewer","sparcl") ) ) ) 
 cat("[ \x1B[92mDONE\x1B[39m ]\n", stderr())
 
 cat("[R] importing scores...", stderr())
@@ -268,19 +300,14 @@ md[ cbind( d$y , d$x ) ] <- as.numeric( d$score )
 # get unique names 
 # d$Xname is of format "pp/PROTEIN_CELL_COORDINATES.pp"
 # these next 5 lines pull out PROTEIN, which also 
-# includes known strucutred RNA controls 
-ids <- unique(d$x_name)
-ids <- sapply(strsplit(as.character(ids),"/"), "[", 2)
-ids <- sapply(strsplit(as.character(ids),"\\."), "[", 1)
-
-#get colors
-palette(rainbow(length(unique(ids))))
-colourMap <- cbind( unique(ids), palette() )
-famColours <- merge(d$x_name, colourMap, by=1)
+# includes structured RNA controls and decoys
+ids <- read.table("ids.txt", header=F)
+#ids <- sapply(strsplit(as.character(ids),"/"), "[", 2)
+#ids <- sapply(strsplit(as.character(ids),"\\."), "[", 1)
 
 # convert to dist
-rownames(md) <- ids
-colnames(md) <- ids
+rownames(md) <- ids$V1
+colnames(md) <- ids$V1
 D <- as.dist( md )
 cat("[ \x1B[92mDONE\x1B[39m ]\n", stderr())
 
@@ -307,8 +334,7 @@ cat("[ \x1B[92mDONE\x1B[39m ]\n", stderr())
 
 cat("[R] performing density-based clustering ...", stderr())
 #kNNdistplot(D,k=5)
-#DB <- dbscan(D,eps=0.44,minPts=5)
-#O <- optics(D, eps=0.5, minPts=5, search="dist")
+Oc <- opticsXi( optics(D, eps=1, minPts=5, search="dist") , xi = 0.006, minimum=T)
 
 printClust <- function( O ) {
   for (cl in 1:NumClust) {
@@ -316,16 +342,12 @@ printClust <- function( O ) {
     print(labels(D)[ O$cluster == cl ]) ;
   }
 }
+# printClust(Oc)
+# plot(Oc)
+# pdf("reachability_plot_optics_xi.pdf")
+#   plot(Oc, lwd=0.5)
+# dev.off()
 
-Oc <- opticsXi( optics(D, eps=1, minPts=5, search="dist") , 
-		xi = 0.01, minimum=T)
-printClust(Oc)
-plot(Oc)
-pdf("reachability_plot_optics_xi.pdf")
-  plot(Oc, lwd=0.5)
-dev.off()
-
-cat("[ \x1B[92mDONE\x1B[39m ]\n", stderr())
 
 # # get a list of optics clusters
 # NumClust <- max(Oc$clusters)
@@ -334,117 +356,161 @@ cat("[ \x1B[92mDONE\x1B[39m ]\n", stderr())
 # 	c[[cl+1]] <- labels(D)[ Oc$cluster == cl ]
 # }
 
-mp <- c(3,4,5,6) 
-xi <- c(0.0001,0.0005,0.001,0.005,0.01,0.05)
-out <- list
-cat("minPts","Xi","clusters","TP","TN","FP","FN","SENS","SPEC","Minimum","\n",sep="\t", file="cluster_bench.tsv")
-for (MP in mp ) {
-	for ( Xi in xi ) {
-		for ( m in c(TRUE,FALSE)) {
-			#Oc <- opticsXi( optics(D, eps=1, minPts=MP, search="dist") , xi = Xi , minimum=T)
-			Oc <- opticsXi( optics(D, eps=1, minPts=MP, search="dist") , xi = Xi , minimum = m )
-			# Get counts of each sequence class
-			idT <- table( sapply( strsplit( labels( D ), "_"), "[", 1 ) )
-			c <- list
-			TP <- list
-			FP <- list
-			NumClust <- max(Oc$cluster)
-			i <- 1 
-			SENS <- 0
-			SPEC <- 0 
-			best <- 0
-			clusters <- 0
-			v <- NA
-			if ( m == TRUE )
-				first <- 0
-			else
-				first <- 1
-			for (cl in first:NumClust) {
-				l <- length(  Oc[Oc$cluster == cl] )
-				# extract non-null clusters
-				if ( l > 0  ) {
-					clusters <- clusters + 1
-					#cat("======= Cluster ",i, "has ",l, "elements =======\n")
-					c[[ i ]] <- labels(D)[ Oc$cluster == cl ]
-					# get a vector of names
-					v <- sapply( strsplit( 
-							as.character( c[[ i ]] ), "_"), "[", 1 ) 
-					t <- sort( table( v ), decreasing=T )
-					best <- as.integer( t[1] )
-					# Ignore cluster if it has >1 major representative
-					if ( ( !is.na( best ) && is.na( as.integer(t[2]))) || as.integer( t[2] ) < best ) {
-						#Get TP and FP
-						cID <- names( t[ 1 ] )
-						if ( cID == "shuffled" ) {
-							FN <- length(v)-best
-							TN <- best
-							#cat( cID,TN,FN,"\n",sep="\t")
-						}
-						else {
-							TP[[ i ]] <- best
-							FP[[ i ]] <- length(v)-best
-							#cat( cID,best,length(v)-best,"\n",sep="\t")
-						}
-						i <- i + 1
-					}
-				}
-			}
-			SENS = sum( TP, na.rm = T ) / ( sum( TP, na.rm = T ) +FN )
-			SPEC = TN/(sum( FP, na.rm = T  ) + TN )
-			cat(MP,Xi,clusters,sum(TP, na.rm = T ),TN,sum(FP,na.rm = T ),FN,SENS,SPEC,m,"\n",sep="\t", file="cluster_bench.tsv", append=T)
-			#out[[ i ]] <- c(MP, Xi, clusters, SENS, SPEC, m)
-		}
+#===============================================
+# 	   		CLUSTER EXTRACTION
+# Based on eCLIP-ECS data and merged RFAM benchmark parameters
+dir.create( file.path( getwd(), "clusters" ), showWarnings = FALSE)
+setwd("clusters")
+NumClust <- max(Oc$cluster)
+clusters <- 0 #make this 1 if optics(... , minimum=F )
+for (cl in 1:NumClust) {
+	l <- length(  Oc[Oc$cluster == cl] )
+	# extract non-null clusters
+	if ( l > 0  ) {
+		sequences <- labels(D)[ Oc$cluster == cl ] 
+		v <- sapply( strsplit( as.character( sequences ), "_"), "[", 1 ) 
+		filename <- paste( "cluster_",cl,".txt", sep="" )
+		write.table( sequences, file=filename , col.names=F, row.names=F, quote=F )
+		clusters <- clusters + 1
 	}
 }
-gg <- read.table("cluster_bench.tsv",header=T)
-# Youden's J statistic can be calculated in bash with somethig like: 
-# tail -n +2 cluster_bench.tsv | awk 'OFS="\t" {print $1,$2,$10,$3,$8,$9,(($4/($4+$7))+($5/($5+$6))-1)}' | sort -k 7rn
+cat("Found ",clusters," clusters\n")
 
-colXi <- cbind (unique(gg$Xi), palette(rainbow(6))  )
-p <- ggplot(gg) + 
-	geom_point( data=subset( gg, Minimum == TRUE), aes( 1-SPEC, SENS, color=factor(Xi)
-		, shape=factor(minPts)), size=2, stroke=1 ) + 
-	geom_point(  data=subset( gg, Minimum==FALSE), aes( 1-SPEC, SENS, color=factor(Xi), fill=factor(Xi)
-		, shape=factor(minPts)), size=2, stroke=1, show.legend=F ) + 
-	scale_y_continuous(lim=c(0,1),breaks=seq(0,1,0.1)) + 
-	scale_x_continuous(lim=c(0,1),breaks=seq(0,1,0.1)) +
-	scale_shape_manual(values=c(21,22,23,24,25)) +
-	scale_color_manual( values=palette(rainbow(6)), name="Steepness\nthreshold Xi") + 
-	scale_fill_manual(values=palette(rainbow(6))) +
-	labs( title="Optics clustering accuracy",
-	x = "False positive rate",
-	y = "Sensitivty",
-	color = "Steepness\nthreshold Xi", 
-	shape = "Minimum\npoints") +
-	geom_abline(intercept=0, slope=1, linetype=3) 
+cat("[ \x1B[92mDONE\x1B[39m ]\n", stderr())
 
-l <- ggplot(gg, aes(factor(Xi), clusters, group=interaction(minPts,Minimum), shape=factor(Minimum), color=factor(minPts))) + 
-	geom_line(aes(linetype=factor(Minimum))) + 
-	geom_point(size=2, aes(stroke=1)) +
-	geom_abline(intercept=33, slope=0, linetype=2) + 
-	guides(shape=F) + 
-	labs( title="Optics clustering fragmentation",
-		x = "Steepness threshold Xi",
-		y = "# of clusters",
-		color = "Minimum\npoints",
-		linetype = "Minimum\nCluster\nExtraction") +
-	theme_bw()
-
-
-#FN <- list(FN, (as.integer( idT[ names(t[1]) ] ) - best ))
-			#TN <- TN[[ i ]] <- length(ids) - TP - FP - FN)
-			#cat( "TP ",TP," FP ",FP," TN ",TN," FN ",FN )
-			#cat( " SENS ",TP/(TP+FN)," SPEC ",TN/(TN+FP),"\n")
-
-sort( as.data.frame( table(  ) )$Freq , decreasing=T)[1]
-
+# #===============================================
+# # 	 Plot the reachability w/cluster numbers
+# plot(Oc) 
+# y <- 0.52 #
+# sorted <- sort(unique(Oc$cluster))
+# for (x in sorted)  {
+# 	if ( x != 0) {
+# 		if (  !is.na( as.integer(t[2])) && as.integer( t[2] ) == best ) 
+# 			n <- paste( names(t[1]), "-", names(t[2]), sep="" )
+# 		else
+# 			n <- names(t[1]) 
+# 		start <- Oc$clusters_xi$start[  Oc$clusters_xi$cluster_id == x ]	
+# 		top <- Oc$reachdist[ start +2 ]
+# 		text( start ,  y,  paste(x,n) , col=x+1 , cex=0.9, pos=4, offset=0, srt=25)
+# 		segments( start +2 , max( 0.5, top +0.02 ) , 
+# 				  start +2, y, 
+# 				  col=x+1, lwd=1.5 )
+# 		if ( y < 0.61 ) {
+# 			y <- y + 0.033
+# 		}
+# 		else {
+# 			y <- 0.52
+# 		}
+# 		v <- sapply( 
+# 				strsplit(
+# 				 	as.character( 
+# 				 		labels(D)[ Oc$cluster == x ] ), "_"), "[", 1 ) 
+# 		t <- sort( table( v ), decreasing=T )
+# 		best <- as.integer( t[1] )
+# 		# Ignore cluster if it has >1 major representative
+# 	}
+# }
+# #===============================================
+# # 	clustering accuracy benchmarking
+# mp <- c(3,4,5,6) 
+# #Avoid more than 5, unless bypassing the "Minimum" option
+# xi <- c(0.005,0.006,0.007,0.008,0.009)
+# out <- list
+# cat("minPts","Xi","clusters","TP","TN","FP","FN","SENS","SPEC","Minimum","\n",sep="\t", file="cluster_bench.tsv")
+# for (MP in mp ) {
+# 	for ( Xi in xi ) {
+# 		for ( m in c(TRUE,FALSE)) {
+# 			#Oc <- opticsXi( optics(D, eps=1, minPts=MP, search="dist") , xi = Xi , minimum=T)
+# 			Oc <- opticsXi( optics(D, eps=1, minPts=MP, search="dist") , xi = Xi , minimum = m )
+# 			# Get counts of each sequence class
+# 			idT <- table( sapply( strsplit( labels( D ), "_"), "[", 1 ) )
+# 			C <- list
+# 			TP <- list
+# 			FP <- list
+# 			NumClust <- max(Oc$cluster)
+# 			i <- 1 
+# 			SENS <- 0
+# 			SPEC <- 0 
+# 			best <- 0
+# 			clusters <- 0
+# 			v <- NA
+# 			if ( m == TRUE )
+# 				first <- 0
+# 			else
+# 				first <- 1
+# 			for (cl in first:NumClust) {
+# 				l <- length(  Oc[Oc$cluster == cl] )
+# 				# extract non-null clusters
+# 				if ( l > 0  ) {
+# 					clusters <- clusters + 1
+# 					#cat("======= Cluster ",i, "has ",l, "elements =======\n")
+# 					C[[ i ]] <- labels(D)[ Oc$cluster == cl ]
+# 					# get a vector of names
+# 					v <- sapply( strsplit( 
+# 							as.character( C[[ i ]] ), "_"), "[", 1 ) 
+# 					t <- sort( table( v ), decreasing=T )
+# 					best <- as.integer( t[1] )
+# 					# Ignore cluster if it has >1 major representative
+# 					if ( ( !is.na( best ) && is.na( as.integer(t[2]))) || as.integer( t[2] ) < best ) {
+# 						#Get TP and FP
+# 						cID <- names( t[ 1 ] )
+# 						if ( cID == "shuffled" ) {
+# 							FN <- length(v)-best
+# 							TN <- best
+# 							#cat( cID,TN,FN,"\n",sep="\t")
+# 						}
+# 						else {
+# 							TP[[ i ]] <- best
+# 							FP[[ i ]] <- length(v)-best
+# 							#cat( cID,best,length(v)-best,"\n",sep="\t")
+# 						}
+# 						i <- i + 1
+# 					}
+# 				}
+# 			}
+# 			SENS = sum( TP, na.rm = T ) / ( sum( TP, na.rm = T ) +FN )
+# 			SPEC = TN/(sum( FP, na.rm = T  ) + TN )
+# 			cat(MP,Xi,clusters,sum(TP, na.rm = T ),TN,sum(FP,na.rm = T ),FN,SENS,SPEC,m,"\n",sep="\t", file="cluster_bench.tsv", append=T)
+# 			#out[[ i ]] <- c(MP, Xi, clusters, SENS, SPEC, m)
+# 		}
+# 	}
+# }
 # calculate main cluster component (TP)
 # calculate cluster components not in cluster (FN)
 # calculate others in cluster (FP)
 # calculate others not in cluster (TN)
 
+# gg <- read.table("cluster_bench.tsv",header=T)
+# # Youden's J statistic can be calculated in bash with somethig like: 
+# # tail -n +2 cluster_bench.tsv | awk 'OFS="\t" {print $1,$2,$10,$3,$8,$9,(($4/($4+$7))+($5/($5+$6))-1)}' | sort -k 7rn
+# p <- ggplot(gg) + 
+# 	geom_point( data=subset( gg, Minimum == TRUE), aes( 1-SPEC, SENS, color=factor(Xi)
+# 		, shape=factor(minPts)), size=2, stroke=1 ) + 
+# 	geom_point(  data=subset( gg, Minimum==FALSE), aes( 1-SPEC, SENS, color=factor(Xi), fill=factor(Xi)
+# 		, shape=factor(minPts)), size=2, stroke=1, show.legend=F ) + 
+# 	scale_y_continuous(lim=c(0,1),breaks=seq(0,1,0.1)) + 
+# 	scale_x_continuous(lim=c(0,1),breaks=seq(0,1,0.1)) +
+# 	scale_shape_manual(values=c(21,22,23,24,25)) +
+# 	scale_color_manual( values=palette(rainbow(6)), name="Steepness\nthreshold Xi") + 
+# 	scale_fill_manual(values=palette(rainbow(6))) +
+# 	labs( title="Optics clustering accuracy",
+# 	x = "False positive rate",
+# 	y = "Sensitivty",
+# 	color = "Steepness\nthreshold Xi", 
+# 	shape = "Minimum\npoints") +
+# 	geom_abline(intercept=0, slope=1, linetype=3) 
 
-
+# l <- ggplot(gg, aes(factor(Xi), clusters, group=interaction(minPts,Minimum), shape=factor(Minimum), color=factor(minPts))) + 
+# 	geom_line(aes(linetype=factor(Minimum))) + 
+# 	geom_point(size=2, aes(stroke=1)) +
+# 	geom_abline(intercept=length(idT), slope=0, linetype=2) + 
+# 	guides(shape=F) + 
+# 	labs( title="Optics clustering fragmentation",
+# 		x = "Steepness threshold Xi",
+# 		y = "# of clusters",
+# 		color = "Minimum\npoints",
+# 		linetype = "Minimum\nCluster\nExtraction") +
+# 	theme_bw()
 
 
 #plot(cut(hcd, h=75)$lower[[2]], 
@@ -471,10 +537,27 @@ EOF
 Rscript --vanilla clustering.R
 >&2 echo -e "[*] exiting :) " && exit 0 
 
-=======
- ${BASEDIR}/../worker.sge ${1}"
-JID=$( $CMD ) 
-echo "launched: "$CMD 
-JID=$( echo ${JID} | cut -d "." -f 1 )
-#qsub -hold_jid $JID -V -o ./$1/logs/dotaligner.$(date +"%d%m%y-%H%M").clean -cwd -N clean_dotalnr -pe smp 1 -l mem_requested=1G,h_vmem=1G -j y -b y ./cleanup.sge $1 dotaligner
->>>>>>> 8d5862964db2eeac240e465312a44c46c9723858
+
+# extract fasta records from clusters
+for file in cluster_*.txt ; do 
+ mkdir ${file%*.txt}
+ #change this to mv ? 
+ cp $file ${file%*.txt}/
+ cd ${file%*.txt}
+ #TO DO filter out clusters with identical peaks 
+ sed -e 's/_dp\.pp//g' -e 's/_/\.*/g' $file | while read line ; do echo '>'$line ; done > temp 
+ grep -A 1 -f temp ../../../../filtered_peaks_RNAbound_ge75_le200_controls.fasta  | grep -v -e '--' > ${file%*.txt}.fasta  
+rm temp
+~/apps/locarna-1.8.11/bin/mlocarna --probabilistic --iterations=10 \
+  --consistency-transformation --threads=6 --noLP ${file%*.txt}.fasta  
+ cd ${file%*.txt}.out 
+  ~/apps/ViennaRNA-2.2.10/bin/RNAalifold --color --aln -r results/result_prog.aln
+  mv alirna.ps ${file%*.txt}_rna.ps
+  mv aln.ps ${file%*.txt}_aln.ps
+ cd ../..
+ rm $file 
+done
+
+
+
+
